7a4f2c1764b7f859ca2327ed3034a9cad5bd4ac4
Pavan Kumar <surisettypavankumar999@gmail.com>
2020-08-26 11:53:15 +0530

    for

---
+++ ifAndFor.js
@@ -0,0 +1,97 @@
+// var age = 22;
+
+// if (age > 18) {
+//   console.log("you are eligible to vote");
+// } else {
+//   console.log("you are not eligible");
+// }
+
+// var age = 14;
+
+// if (age > 18) {
+//   console.log("you are eligible to vote");
+// } else {
+//   console.log("you are not eligible");
+// }
+
+// console.log(age % 5);
+
+// var number = 1b ;
+
+// if (number % 2 == 1) {
+//   console.log("number is odd number");
+// } else {
+//   console.log("number is even number");
+// }
+// function checkHeight(height) {
+//   if (height < 5) {
+//     console.log("short");
+//   } else if (height < 7) {
+//     console.log("Normal");
+//   } else {
+//     console.log("Long");
+//   }
+// }
+// checkHe1ight(4);
+// checkHeight(5);
+// checkHeight(7);
+// checkHeight(6);
+// checkHeight(10);
+
+// let person = {
+//   name: "Rishi",
+//   gender: "male",
+// };
+
+// let person2 = {
+//   name: "Reshma",
+//   gender: "female",
+// };
+
+// function checkGender(p) {
+//   if (p.gender == "male") {
+//     console.log("Mr " + p.name + " Kumar");
+//   } else {
+//     console.log("Ms " + p.name + " Kumari");
+//   }
+// }
+
+// checkGender(person2);
+// checkGender({ name: "Vijay", gender: "male" });
+// checkGender(person);
+
+// function display(x) {
+//   var result = x > 5 ? "x is greater than 5" : "x is lesser than 5";
+//   console.log(result);
+// }
+
+// display(6);
+// display(4);
+// display(5);
+
+// const name = "Rishi";
+// function greeting() {
+//   console.log("HIi " + name);
+// }
+
+// greeting();
+
+// for (i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for ( var i = 0; i < 5; i++) {
+//   console.log("Hii Rishi");
+// }
+
+// // function display() {
+//   let friends = ["vijay", "rishi", "harish", "Kumar"];
+//   for (i = 0; i < friends.length; i++) {
+//     console.log(friends[i]);
+//   }
+// }
+
+// display();
+
+let friends = ["vijay", "rishi", "harish", "Kumar"];
+friends.forEach((f) => console.log(f));
