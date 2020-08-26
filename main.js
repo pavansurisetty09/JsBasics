ca74ec7a98f6879b3fddac1d26fddca74582fb73
Pavan Kumar <surisettypavankumar999@gmail.com>
2020-08-26 11:56:09 +0530

    for

---
+++ main.js
@@ -0,0 +1,84 @@
+// const name = "Rishi";
+
+// console.log(name);
+
+// console.log("Hii " + name);
+
+// var a = 2;
+// var b = 10;
+
+// console.log(a + b);
+
+// console.log(a - b);
+
+// console.log(a * b);
+
+// console.log(a / b);
+
+// function greeting(name) {
+//   console.log("Hii " + name + " Good Morning");
+// }
+
+// greeting("Vijay");
+// greeting("Paramesh");
+
+// function increment(i) {
+//   // console.log(i + 1);
+//   return i + 1;
+// }
+
+// increment(5);
+
+// function add(a = 0, b = 0) {
+//   console.log(a + b);
+// }
+// add(increment(5), increment(5));
+// add();
+
+// let friends = ["Vijay", "sai", "Harish"];
+// console.log(friends[0]);
+
+// console.log(friends[friends.length - 1]);
+
+// console.log(friends.length);
+
+// friends[3] = "paramesh";
+
+// console.log(friends);
+
+// friends[friends.length] = "Ramesh";
+
+// console.log(friends);
+// friends.push("praveen");
+
+// console.log(friends);
+
+// friends[2] = "Rishi";
+
+// console.log(friends);
+
+// // friends[0] = "vijay kumar";
+
+// friends[0] = friends[0] + " kumar";
+// console.log(friends);
+
+// let person = {
+//   name: "vijay",
+//   age: 22,
+//   location: "Anakapalli",
+// };
+
+// // console.log(person);
+
+// console.log(person.name);
+
+// console.log((person.age = person.age - 1));
+// console.log(person);
+
+// person.phone = 7995533369;
+
+// console.log(person);
+
+const name = "Rishi";
+
+console.log(name);
---
+++ table.js
@@ -0,0 +1,9 @@
+// console.log("1 x 1 = 1");
+
+// for (i = 1; i < 13; i++) {
+//   console.log("1 x " + i + " = " + i * 1);
+// }
+
+// for (i = 1; i < 13; i++) {
+//   console.log("5 x " + i + " = " + i * 5);
+// }
